7bc47db73adc73627bd2316266c902de20df4fb8
John Uel Mangaba <johnuelmangaba@gmail.com>
2022-07-29 08:58:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "https://coffee-plum.vercel.app/api/login",
+        source: "/api/login",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
