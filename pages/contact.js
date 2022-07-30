33f444b9d08386d6c72434607d9e26aac20bf447
John Uel Mangaba <johnuelmangaba@gmail.com>
2022-07-30 14:03:08 +0800

    Update contact.js
    change class name

--- pages/contact.js
+++ pages/contact.js
@@ -3,39 +3,39 @@ import styles from '../styles/Contact.module.css'
 export default function Contact () {
     return(
         <div className={styles.container}>
-            <div class="row">
+            <div className={styles.row}>
 			<h1>Contact us</h1>
             </div>
-            <div class="row">
+            <div className={styles.row}>
                     <h4 style="text-align:center">We'd love to hear from you!</h4>
             </div>
-            <div class="row input-container">
-                    <div class="col-xs-12">
-                        <div class="styled-input wide">
+            <div className={styles.input-container}>
+                    <div className="col-xs-12">
+                        <div className="styled-input wide">
                             <input type="text" required />
                             <label>Name</label> 
                         </div>
                     </div>
-                    <div class="col-md-6 col-sm-12">
-                        <div class="styled-input">
+                    <div className="col-md-6 col-sm-12">
+                        <div className="styled-input">
                             <input type="text" required />
                             <label>Email</label> 
                         </div>
                     </div>
-                    <div class="col-md-6 col-sm-12">
-                        <div class="styled-input" style="float:right;">
+                    <div className="col-md-6 col-sm-12">
+                        <div className="styled-input" style="float:right;">
                             <input type="text" required />
                             <label>Phone Number</label> 
                         </div>
                     </div>
-                    <div class="col-xs-12">
-                        <div class="styled-input wide">
+                    <div className="col-xs-12">
+                        <div className="styled-input wide">
                             <textarea required></textarea>
                             <label>Message</label>
                         </div>
                     </div>
-                    <div class="col-xs-12">
-                        <div class="btn-lrg submit-btn">Send Message</div>
+                    <div className="col-xs-12">
+                        <div className="btn-lrg submit-btn">Send Message</div>
                     </div>
             </div>
         </div>
